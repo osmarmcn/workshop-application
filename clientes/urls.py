2a0529b5f94391f6411844edc0bab291cf5eb13b
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-03-18 18:25:47 -0300

    criando models cliente e carro

--- clientes/urls.py
+++ clientes/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+
     path('', views.clientes, name='clientes')
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -3,5 +3,17 @@ from django.http import HttpResponse
 
 
 def clientes(request):
-    return render(request, 'clientes.html')
+    if request.method == 'GET':
+        return render(request, 'clientes.html')
+    
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST,getlist('carro')
+        placas = request.POST,getlist('placa')
+        ano = request.POST.getlist('ano')
+        
+
 
