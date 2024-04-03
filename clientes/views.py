b84d93b6eeabd4b12a632f555bc4130f1bbdc614
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-04-03 12:45:33 -0300

    devolvendo so dados ao  cliente

--- clientes/views.py
+++ clientes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Cliente, Carro
 import re
+from django.core import serializers
+import json
 
 
 def clientes(request):
@@ -50,11 +52,13 @@ def clientes(request):
 def att_cliente(request):
     id_cliente = request.POST.get('id_cliente')
     cliente = Cliente.objects.filter(id=id_cliente)
-    print(cliente)
-    # print('teste')
+   
+    # devolver os dados do cliente
+    cliente_json = json.loads(serializers.serialize('json', cliente))[0] ['fields']
+    # print(cliente_json)
 
 
-    return JsonResponse({"teste": 1})
+    return JsonResponse(cliente_json)
         
 
 
